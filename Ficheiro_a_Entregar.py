c99a7761f2b3156c6aa7860c0a0f41be3d3925c7
Milord10 <miguel.diniz@live.com.pt>
2018-12-06 15:57:45 +0000

    Update Ficheiro_a_Entregar.py

--- Ficheiro_a_Entregar.py
+++ Ficheiro_a_Entregar.py
@@ -14,6 +14,7 @@ Plo0 = 5
 Plomax = 100
 Mlo = 0.0005
 
+Months=int(input())
 Eco={"Pce0":50000,"Pco0":50,"Plo0":5}
 def evoluir_populacao_cenouras (Pce,Rce,Pcemax,Mce,Pco):
     PceAfter=Pce+(Rce*Pce)*(1-(Pce/Pcemax))-(Mce*Pco)
@@ -34,6 +35,20 @@ def evoluir_populacao_lobos (Plo,Pco,Mco,Plomax,Mlo):
     return PloAfter
 
 #Aqui começa a Etapa 4
+def simular_populacoes (num_meses,param):
+    param={"Pce0":50000,"Pco0":50,"Plo0":5}
+    Iteration=0
+    while Iteration<=num_meses:      #Isto está tudo errado, volta sempre ao valor inicial Xxx0
+        CenourasIntermidiate=evoluir_populacao_cenouras (param[Pce0],Rce,Pcemax,Mce,param[Pco0])
+        CoelhosIntermidiate=evoluir_populacao_coelhos (param[Pco0], param[Pce0],Mce,Pcomax,Mco, param[Plo])
+        LobosIntermidiate=evoluir_populacao_lobos (param[Plo],param[Pco],Mco,Plomax,Mlo)
+        Iteration+=1
+    param[Pce0]=CenourasIntermidiate
+    param[Pco0]=CoelhosIntermidiate
+    param[Plo0]=LobosIntermidiate
+    FinalString=("Mês "+num_mes+" : "+param[Pce0]+" cenouras, "+param[Pco0]+" coelhos,"+param[Plo0]+" lobos")
+    return FinalString
+print(simular_populacoes (Months,Eco))
 #Aqui começa a Etapa 5
 #Aqui começa a Etapa 6
 #Aqui começa a Etapa 7
