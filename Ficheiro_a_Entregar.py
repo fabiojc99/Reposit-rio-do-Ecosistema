f5c8dbe57f1359e3f979dc65356af3d21869110d
Milord10 <miguel.diniz@live.com.pt>
2018-12-06 11:29:06 +0000

    Update Ficheiro_a_Entregar.py

--- Ficheiro_a_Entregar.py
+++ Ficheiro_a_Entregar.py
@@ -3,16 +3,16 @@
 #####
 
 
-#Pce0=50000
-#Rce=1
-#PCemax=100000
-#Mce = 5
-#Pco0 = 50
-#PComax = 1000
-#Mco = 2
-#Plo0 = 5
-#PLomax = 100
-#Mlo = 0.0005
+Pce0=50000
+Rce=1
+Pcemax=100000
+Mce = 5
+Pco0 = 50
+Pcomax = 1000
+Mco = 2
+Plo0 = 5
+Plomax = 100
+Mlo = 0.0005
 
 Ecosistema={"Pce0":50000,"Pco0":50,"Plo0":5}
 def evoluir_populacao_cenouras (Pce,Rce,Pcemax,Mce,Pco):
