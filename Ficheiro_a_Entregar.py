726bbbe3ddd3ee1a435a94a016fae456716a0d3d
Milord10 <miguel.diniz@live.com.pt>
2018-12-06 14:45:38 +0000

    Update Ficheiro_a_Entregar.py

--- Ficheiro_a_Entregar.py
+++ Ficheiro_a_Entregar.py
@@ -14,7 +14,7 @@ Plo0 = 5
 Plomax = 100
 Mlo = 0.0005
 
-Ecosistema={"Pce0":50000,"Pco0":50,"Plo0":5}
+param={"Pce0":50000,"Pco0":50,"Plo0":5}
 def evoluir_populacao_cenouras (Pce,Rce,Pcemax,Mce,Pco):
     PceAfter=Pce+(Rce*Pce)*(1-(Pce/Pcemax))-(Mce*Pco)
     return PceAfter
