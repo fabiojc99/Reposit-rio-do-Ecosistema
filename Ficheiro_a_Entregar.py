692770465778bc12981fbafd45ac8ec85e013d15
Milord10 <miguel.diniz@live.com.pt>
2018-12-07 17:46:15 +0000

    Update Ficheiro_a_Entregar.py

--- Ficheiro_a_Entregar.py
+++ Ficheiro_a_Entregar.py
@@ -3,52 +3,67 @@
 #####
 
 
-Pce0=50000
-Rce=1
-Pcemax=100000
-Mce = 5
-Pco0 = 50
-Pcomax = 1000
-Mco = 2
-Plo0 = 5
-Plomax = 100
-Mlo = 0.0005
+#Pce0=50000
+#Rce=1
+#Pcemax=100000
+#Mce = 5
+#Pco0 = 50
+#Pcomax = 1000
+#Mco = 2
+#Plo0 = 5
+#Plomax = 100
+#Mlo = 0.0005
 
 Months=int(input())
 Eco={"Pce0":50000,"Pco0":50,"Plo0":5}
 def evoluir_populacao_cenouras (Pce,Rce,Pcemax,Mce,Pco):
     PceAfter=Pce+(Rce*Pce)*(1-(Pce/Pcemax))-(Mce*Pco)
-    return PceAfter
-print(evoluir_populacao_cenouras(5000,1,100000,5,50))   
+    return round(PceAfter)
 
 #Aqui começa a Etapa 2
 
 def evoluir_populacao_coelhos (Pco,Pce,Mce,Pcomax,Mco,Plo):
-    Nco=(Pce/Mco*Plo)
-    PcoAfter=Pco+(Nco*(1-(Pco/Pcomax)))-(Mco*Plo)
-    return PcoAfter
+    Nco=(Pce/Mce*Pco)
+    PcoAfter=Pco+Nco*(1-(Pco/Pcomax))-(Mco*Plo)
+    return round(PcoAfter)
 
 #Aqui começa a Etapa 3
 def evoluir_populacao_lobos (Plo,Pco,Mco,Plomax,Mlo):
     Nlo=(Pco/(Mco*Plo))
     PloAfter=Plo+Nlo*(1-(Plo/Plomax))-(Mlo*Plo)
-    return PloAfter
+    return round(PloAfter)
 
 #Aqui começa a Etapa 4
 def simular_populacoes (num_meses,param):
-    param={"Pce0":50000,"Pco0":50,"Plo0":5}
     Iteration=0
-    while Iteration<=num_meses:      #Isto está tudo errado, volta sempre ao valor inicial Xxx0
-        CenourasIntermidiate=evoluir_populacao_cenouras (param[Pce0],Rce,Pcemax,Mce,param[Pco0])
-        CoelhosIntermidiate=evoluir_populacao_coelhos (param[Pco0], param[Pce0],Mce,Pcomax,Mco, param[Plo])
-        LobosIntermidiate=evoluir_populacao_lobos (param[Plo],param[Pco],Mco,Plomax,Mlo)
+    Pce0=5000
+    Rce=1
+    Pcemax=1000000
+    Mce=5
+    Pco0=50
+    Pcomax=1000
+    Mco=2
+    Plo0=5
+    Plomax=100
+    Mlo=0.0005
+    while Iteration<num_meses: #Isto está tudo errado, volta sempre ao valor inicial Xxx0
+        CenourasIntermidiate=evoluir_populacao_cenouras (Pce0,Rce,Pcemax,Mce,Pco0)
+        CoelhosIntermidiate=evoluir_populacao_coelhos (Pco0,Pce0,Mce,Pcomax,Mco,Plo0)
+        LobosIntermidiate=evoluir_populacao_lobos (Plo0,Pco0,Mco,Plomax,Mlo)
         Iteration+=1
-    param[Pce0]=CenourasIntermidiate
-    param[Pco0]=CoelhosIntermidiate
-    param[Plo0]=LobosIntermidiate
-    FinalString=("Mês "+num_mes+" : "+param[Pce0]+" cenouras, "+param[Pco0]+" coelhos,"+param[Plo0]+" lobos")
+        Pce0=CenourasIntermidiate
+        Pco0=CoelhosIntermidiate
+        Plo0=LobosIntermidiate
+        print(Pce0)
+        print(Pco0)
+        print(Plo0)
+    param[Pce0]=int(Pce0)
+    param[Pco0]=int(Pco0)
+    param[Plo0]=int(Plo0)
+    FinalString=("Mês "+str(num_meses)," : ",str(param[Pce0])," cenouras, ",str(param[Pco0])," coelhos,",(param[Plo0])," lobos")
     return FinalString
 print(simular_populacoes (Months,Eco))
+
 #Aqui começa a Etapa 5
 #Aqui começa a Etapa 6
 #Aqui começa a Etapa 7
