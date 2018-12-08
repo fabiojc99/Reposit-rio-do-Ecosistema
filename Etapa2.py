72bfaa18a88ec355faf7044e7fc0298e919c300f
Milord10 <miguel.diniz@live.com.pt>
2018-12-08 11:06:42 +0000

    Update Etapa2.py

--- Etapa2.py
+++ Etapa2.py
@@ -1,6 +1,6 @@
 
 def evoluir_populacao_coelhos (Pco,Pce,Mce,Pcomax,Mco,Plo):
-    Nco=(Pce/Mce*Pco)
+    Nco=(Pce/(Mce*Pco))
     PcoAfter=Pco+Nco*(1-(Pco/Pcomax))-(Mco*Plo)
     return round(PcoAfter)
 
