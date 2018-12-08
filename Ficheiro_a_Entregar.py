b967a9648455cf1d2af65cc8fa24cfc3d6314506
Milord10 <miguel.diniz@live.com.pt>
2018-12-08 11:34:56 +0000

    Update Ficheiro_a_Entregar.py

--- Ficheiro_a_Entregar.py
+++ Ficheiro_a_Entregar.py
@@ -17,7 +17,7 @@
 Months=int(input())
 Eco={"Pce0":50000,"Pco0":50,"Plo0":5}
 def evoluir_populacao_cenouras (Pce,Rce,Pcemax,Mce,Pco):
-    PceAfter=Pce+(Rce*Pce)*(1-(Pce/Pcemax))-(Mce*Pco)
+    PceAfter=Pce+((Rce*Pce)*(1-(Pce/Pcemax)))-(Mce*Pco)
     return round(PceAfter)
 
 #Aqui começa a Etapa 2
@@ -46,21 +46,19 @@ def simular_populacoes (num_meses,param):
     Plo0=5
     Plomax=100
     Mlo=0.0005
-    while Iteration<num_meses: #Isto está tudo errado, volta sempre ao valor inicial Xxx0
+    while Iteration<num_meses:
         CenourasIntermidiate=evoluir_populacao_cenouras (Pce0,Rce,Pcemax,Mce,Pco0)
         CoelhosIntermidiate=evoluir_populacao_coelhos (Pco0,Pce0,Mce,Pcomax,Mco,Plo0)
         LobosIntermidiate=evoluir_populacao_lobos (Plo0,Pco0,Mco,Plomax,Mlo)
         Iteration+=1
+        print(CenourasIntermidiate)
         Pce0=CenourasIntermidiate
         Pco0=CoelhosIntermidiate
         Plo0=LobosIntermidiate
-        print(Pce0)
-        print(Pco0)
-        print(Plo0)
     param[Pce0]=int(Pce0)
     param[Pco0]=int(Pco0)
     param[Plo0]=int(Plo0)
-    FinalString=("Mês "+str(num_meses)," : ",str(param[Pce0])," cenouras, ",str(param[Pco0])," coelhos,",(param[Plo0])," lobos")
+    FinalString='Mês',str(num_meses),':',str(param[Pce0]),'cenouras,',str(param[Pco0]),'coelhos,',(param[Plo0]),'lobos'
     return FinalString
 print(simular_populacoes (Months,Eco))
 
