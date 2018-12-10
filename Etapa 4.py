9098326a52253a3dde4bfb5a8b65b8caa385cc75
Milord10 <miguel.diniz@live.com.pt>
2018-12-10 19:02:02 +0000

    Update Etapa 4.py

--- Etapa 4.py
+++ Etapa 4.py
@@ -1,14 +1,36 @@
+#Aqui começa a Etapa 4
 def simular_populacoes (num_meses,param):
-    param={"Pce0":50000,"Pco0":50,"Plo0":5}
     Iteration=0
-    while Iteration<=num_meses: #Isto está tudo errado, volta sempre ao valor inicial Xxx0
-        CenourasIntermidiate=evoluir_populacao_cenouras (param[Pce0],Rce,Pcemax,Mce,param[Pco0])
-        CoelhosIntermidiate=evoluir_populacao_coelhos (param[Pco0], param[Pce0],Mce,Pcomax,Mco, param[Plo])
-        LobosIntermidiate=evoluir_populacao_lobos (param[Plo],param[Pco],Mco,Plomax,Mlo)
+    Pce0=50000
+    Rce=1
+    Pcemax=100000
+    Mce=5
+    Pco0=50
+    Pcomax=1000
+    Mco=2
+    Plo0=5
+    Plomax=100
+    Mlo=0.0005
+    EcosystemPython=open( 'Ecosystem.txt','w')
+    if Iteration==0:
+        EcosystemPython.write(('Mês '+str(Iteration)+' : '+str(50000) +' cenouras,'+str(50)+' coelhos,'+str(5)+' lobos')+('\n'))
         Iteration+=1
-    param[Pce0]=CenourasIntermidiate
-    param[Pco0]=CoelhosIntermidiate
-    param[Plo0]=LobosIntermidiate
-    FinalString=("Mês "+num_mes+" : "+param[Pce0]+" cenouras, "+param[Pco0]+" coelhos,"+param[Plo0]+" lobos")
-    return FinalString
+    while Iteration<=num_meses and not Iteration==0 :
+        CenourasIntermidiate=evoluir_populacao_cenouras (Pce0,Rce,Pcemax,Mce,Pco0)
+        CoelhosIntermidiate=evoluir_populacao_coelhos (Pco0,Pce0,Mce,Pcomax,Mco,Plo0)
+        LobosIntermidiate=evoluir_populacao_lobos (Plo0,Pco0,Mco,Plomax,Mlo)
+        Pce0=CenourasIntermidiate
+        Pco0=CoelhosIntermidiate
+        Plo0=LobosIntermidiate
+        EcosystemPython.write(('Mês '+str(Iteration)+' : '+str(Pce0) +' cenouras,'+str(Pco0)+' coelhos,'+str(Plo0)+' lobos')+('\n'))
+        Iteration+=1
+
+    EcosystemPython.close()    
+    param[Pce0]=int(Pce0) #Esta parte do Dicionário pode não ser utilizado na etapa 4 mas pode ser útil a posteriori
+    param[Pco0]=int(Pco0)
+    param[Plo0]=int(Plo0)
+    EcosystemPython=open('Ecosystem.txt','r')
+    FinalEcosystem=EcosystemPython.read()
+    EcosystemPython.close()
+    return FinalEcosystem
 print(simular_populacoes (Months,Eco))
